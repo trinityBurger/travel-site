439110d6d709db753e098f97988dacacdabe6fa0
Trinity <arandomgeek@gmail.com>
2020-07-21 18:25:24 -0500

    start styling about  section

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path")
 
-const postCSSPlugins = [require("postcss-import"), require("postcss-simple-vars"), require("postcss-nested"), require("autoprefixer")]
+const postCSSPlugins = [require("postcss-import"), require("postcss-mixins"), require("postcss-simple-vars"), require("postcss-nested"), require("autoprefixer")]
 
 module.exports = {
   entry: "./app/assets/scripts/App.js",
@@ -8,8 +8,16 @@ module.exports = {
     filename: "bundled.js",
     path: path.resolve(__dirname, "app")
   },
+  devServer: {
+    before: function (app, server) {
+      server._watch("./app/**/*.html")
+    },
+    contentBase: path.join(__dirname, "app"),
+    hot: true,
+    port: 3000,
+    host: "0.0.0.0"
+  },
   mode: "development",
-  watch: true,
   module: {
     rules: [
       {
