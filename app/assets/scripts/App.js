effe2c30eb24ce85b038aa3598a7625455bf2708
Trinity <arandomgeek@gmail.com>
2020-08-08 14:21:25 -0500

    complete reveal on scroll behivior

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,10 @@
 import "../styles/styles.css"
 import MobileMenu from "./modules/MobileMenu"
+import RevealOnScroll from "./modules/RevealOnScroll"
 
 let mobileMenu = new MobileMenu()
+new RevealOnScroll(document.querySelectorAll(".M-featureItem"), 75)
+new RevealOnScroll(document.querySelectorAll(".testimonialItem"), 60)
 
 if (module.hot) {
   module.hot.accept()
