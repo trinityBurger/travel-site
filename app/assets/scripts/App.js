023f6665775393b8e1899693ea8c0762d40c12d6
Trinity <arandomgeek@gmail.com>
2020-08-09 10:49:42 -0500

    complete modal overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,10 +3,27 @@ import MobileMenu from "./modules/MobileMenu"
 import RevealOnScroll from "./modules/RevealOnScroll"
 import StickyHeader from "./modules/StickyHeader"
 
-let stickyHeader = new StickyHeader()
-let mobileMenu = new MobileMenu()
+new StickyHeader()
+new MobileMenu()
 new RevealOnScroll(document.querySelectorAll(".M-featureItem"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonialItem"), 60)
+let modal
+
+document.querySelectorAll(".openModal").forEach(el => {
+  el.addEventListener("click", e => {
+    e.preventDefault()
+    if (typeof modal == "undefined") {
+      import(/* webpackChunkName: "modal" */ "./modules/Modal")
+        .then(x => {
+          modal = new x.default()
+          setTimeout(() => modal.openTheModal(), 20)
+        })
+        .catch(() => console.log("there was a problem"))
+    } else {
+      modal.openTheModal()
+    }
+  })
+})
 
 if (module.hot) {
   module.hot.accept()
